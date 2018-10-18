052517a470508d296991e12c08c2a94f11cab7fc
Elena Dobosariu <elena.dobosariu@yrbrands.com>
2018-10-18 10:14:29 +0200

    gitignore + small change

--- GreenSights/AppDelegate.swift
+++ GreenSights/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		window = UIWindow()
 		window?.makeKeyAndVisible()
 		window?.rootViewController = GSTabBarController()
-		
+		//here
 		return true
 	}
 	
